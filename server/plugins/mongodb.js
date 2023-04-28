05a94b5e53490e6f799e65520ef9736a35c5f049
Qook <anx2030@naver.com>
2023-04-28 17:01:10 +0900

    test: .env

--- server/plugins/mongodb.js
+++ server/plugins/mongodb.js
@@ -4,12 +4,13 @@ const fp = require('fastify-plugin')
 
 const {
     MONGO_INITDB_ROOT_USERNAME,
-    MONGO_INITDB_ROOT_PASSWORD
+    MONGO_INITDB_ROOT_PASSWORD,
+   // MONGO_INITDB_ROOT_HOSTNAME
  } = process.env
  
 module.exports = fp(async function (fastify, opts) {
     fastify.register(require('@fastify/mongodb'), {
         url: 
-        `mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}mongo:27017`
+        `mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@localhost:27017/?authMechanism=DEFAULT`
       })
 })
