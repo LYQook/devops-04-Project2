0af8a4a5421ab61f2ea3dfef1130bbcd050a44c0
Qook <anx2030@naver.com>
2023-04-28 21:31:10 +0900

    fix: .env compse.yml mongodb.js for check link server ans db

--- server/plugins/mongodb.js
+++ server/plugins/mongodb.js
@@ -2,15 +2,32 @@
 
 const fp = require('fastify-plugin')
 
-const {
-    MONGO_INITDB_ROOT_USERNAME,
-    MONGO_INITDB_ROOT_PASSWORD,
-   // MONGO_INITDB_ROOT_HOSTNAME
- } = process.env
- 
+const { MONGO_INITDB_ROOT_USERNAME, MONGO_INITDB_ROOT_PASSWORD, MONGO_INITDB_ROOT_HOSTNAME } = process.env
+
 module.exports = fp(async function (fastify, opts) {
-    fastify.register(require('@fastify/mongodb'), {
-        url: 
-        `mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@localhost:27017/?authMechanism=DEFAULT`
-      })
+  const url = `mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@${MONGO_INITDB_ROOT_HOSTNAME}:27017/?authMechanism=DEFAULT`
+  console.log(url)
+
+  fastify.register(require('@fastify/mongodb'), {
+    forceClose: true,
+    url: url
+  })
 })
+
+
+// 'use strict'
+
+// const fp = require('fastify-plugin')
+
+// const {
+//     MONGO_INITDB_ROOT_USERNAME,
+//     MONGO_INITDB_ROOT_PASSWORD,
+//    // MONGO_INITDB_ROOT_HOSTNAME
+//  } = process.env
+ 
+// module.exports = fp(async function (fastify, opts) {
+//     fastify.register(require('@fastify/mongodb'), {
+//         url: 
+//         `mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@localhost:27017/?authMechanism=DEFAULT`
+//       })
+// })
