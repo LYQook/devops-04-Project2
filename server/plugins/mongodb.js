ce363273b9add439721c0bf9dfdbf875abc61675
Qook <anx2030@naver.com>
2023-04-30 01:11:44 +0900

    move: .en fix: mongodb.js

--- server/plugins/mongodb.js
+++ server/plugins/mongodb.js
@@ -1,13 +1,14 @@
 'use strict'
 
 const fp = require('fastify-plugin')
-
+require('dotenv').config()
 const { MONGO_INITDB_ROOT_USERNAME, MONGO_INITDB_ROOT_PASSWORD, MONGO_INITDB_ROOT_HOSTNAME } = process.env
 
 module.exports = fp(async function (fastify, opts) {
+
   const url = `mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@${MONGO_INITDB_ROOT_HOSTNAME}:27017/?authMechanism=DEFAULT`
   console.log(url)
-  console.log('qooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooook')
+
   fastify.register(require('@fastify/mongodb'), {
     forceClose: true,
     url: url
