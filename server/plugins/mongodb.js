73779317d685004680d2ebc60731c009879e5bc4
Qook <anx2030@naver.com>
2023-05-01 20:51:21 +0900

    fix mongodb.js

--- server/plugins/mongodb.js
+++ server/plugins/mongodb.js
@@ -2,13 +2,12 @@
 
 const fp = require('fastify-plugin')
 require('dotenv').config()
-const { MONGO_INITDB_ROOT_USERNAME, MONGO_INITDB_ROOT_PASSWORD } = process.env
-//const { MONGO_INITDB_ROOT_USERNAME, MONGO_INITDB_ROOT_PASSWORD, MONGO_INITDB_ROOT_HOSTNAME } = process.env
+
+const { MONGO_INITDB_ROOT_USERNAME, MONGO_INITDB_ROOT_PASSWORD, MONGO_INITDB_ROOT_HOSTNAME } = process.env
 
 module.exports = fp(async function (fastify, opts) {
 
-  const url = `mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@mongo:27017/?authMechanism=DEFAULT`
-//  const url = `mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@${MONGO_INITDB_ROOT_HOSTNAME}:27017/?authMechanism=DEFAULT`
+const url = `mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@${MONGO_INITDB_ROOT_HOSTNAME}:27017/?authMechanism=DEFAULT`
   console.log(url)
 
   fastify.register(require('@fastify/mongodb'), {
